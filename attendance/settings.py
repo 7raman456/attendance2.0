014c6f03943226ef6be546baac8ba96cd1e541d9
raman <7raman456@gmail.com>
2021-04-07 10:11:20 +0530

    'hbd'

--- attendance/settings.py
+++ attendance/settings.py
@@ -14,7 +14,11 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+ALLOWED_HOSTS = [
+    '0.0.0.0'
+    'ancient-mountain-70111.herokuapp.com',
+    '127.0.0.1'
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
