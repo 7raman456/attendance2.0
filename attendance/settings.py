a7ed1ffd9c9911224c6e86e14b31aa692f9c02db
raman <7raman456@gmail.com>
2021-04-07 10:18:18 +0530

    'hbd'

--- attendance/settings.py
+++ attendance/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 ALLOWED_HOSTS = [
     '0.0.0.0',
     'ancient-mountain-70111.herokuapp.com',
